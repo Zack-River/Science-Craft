b42661b0729cfe202aedb6850c6fc8a40b533161
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2024-12-21 20:16:26 +0200

    Database Creation v1.0

--- ScienceCraft/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ScienceCraft.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
